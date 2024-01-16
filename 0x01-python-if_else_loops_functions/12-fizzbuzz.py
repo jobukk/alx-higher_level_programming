9a1bc998c494f7296039815308f70b49a6589eb8
jobukk <jobkipkurui3843@gmail.com>
2024-01-16 13:11:48 -0800

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,12 +3,12 @@ def fizzbuzz():
     #3 mutiples-Fizz
     #5 multiples - Buzz
     #3 and 5 multiples -FizzBuzz
-    for x in range(1,101):
-        if x % 3 == 0:
-            print("Fizz ", end="")
-        elif x % 5 == 0:
-            print("Buzz ", end="")
-        elif x % 3 == 0 and x % 5 == 0:
-            print("FizzBuzz ", end="")
-        else:
-            print(x, end=" ")
+   for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif x % 3 == 0:
+        print("Fizz", end=" ")
+    elif x % 5 == 0:
+        print("Buzz", end=" ")
+    else:
+        print(x, end=" ")
