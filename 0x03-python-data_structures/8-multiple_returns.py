9d63d6c7f6d82fe18b1fd7dc92a6a645efe9c672
jobukk <jobkipkurui3843@gmail.com>
2024-01-22 14:02:55 -0800

    lists and tuples

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = 0
+    if sentence == "":
+        first_char = None
+        return length, first_char
+    for char in sentence: 
+        length += 1
+    first_char = sentence[0]
+    return length, first_char
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    large = my_list[0] if my_list else None
+    for i in my_list:
+        if i > large:
+            large = i
+    return large        
