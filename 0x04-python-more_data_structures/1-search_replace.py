19959bd9984d7224e67582658906b0163d3266f0
jobukk <jobkipkurui3843@gmail.com>
2024-01-24 10:01:42 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,9 +3,9 @@
 def search_replace(my_list, search, replace):
     """Get the element then replace."""
     new_list = []
-    for elem in range(0,len(my_list)):
-        if my_list[elem] == search:
+    for element in my_list:
+        if element == search:
             new_list.append(replace)
         else:
-            new_list.append(elem)
+            new_list.append(element)
     return new_list
