b627123798f8a919f915aa8df1aa1c3de904fac4
jobukk <jobkipkurui3843@gmail.com>
2024-01-24 11:22:02 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 
-"""Update dictionary."""
 def update_dictionary(a_dictionary, key, value):
     """Fuction to make an update on dictionary."""
     new_dict = a_dictionary.copy()
