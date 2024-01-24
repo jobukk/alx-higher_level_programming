74dcd935673eac482e0fdb1a516e3818da258309
jobukk <jobkipkurui3843@gmail.com>
2024-01-24 11:16:34 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 
-"""Square element in matrix list."""
 def square_matrix_simple(matrix=[]):
-    """Use list comprehension and map to square each element in the matrix."""
+    """
+    Use list comprehension and map
+    to square each element in the matrix.
+    """
     return [list(map(lambda x: x**2, row)) for row in matrix]
