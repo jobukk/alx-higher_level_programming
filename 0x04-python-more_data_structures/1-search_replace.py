40e3186ceedcdad5616b01c46d2324ab1ba5a1ea
jobukk <jobkipkurui3843@gmail.com>
2024-01-24 09:44:40 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 """Search for a value and replace."""
 def search_replace(my_list, search, replace):
     """Get the element then replace."""
-   new_list = my_list[:] 
-   the_index = new_list.index(search)
-   new_list[the_index] = replace
-   return new_list
+    new_list = my_list[:]
+    the_index = new_list.index(search)
+    new_list[the_index] = replace
+    return new_list
