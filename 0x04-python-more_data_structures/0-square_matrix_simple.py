9f2636bf2276086f49865324a1469f8b05e6e1a1
jobukk <jobkipkurui3843@gmail.com>
2024-01-24 09:24:29 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Square element in matrix list."""
 def square_matrix_simple(matrix=[]):
+    """Loop through a matrix and utilize the map() to sqaure each elemt in matrix."""
     arr = []
     for array in matrix:
         squares = map(lambda x : x**2, array)
