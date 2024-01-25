4f2dac2cab815c1c2793d3ff62f1ef3fd628672b
jobukk <jobkipkurui3843@gmail.com>
2024-01-24 20:43:43 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -9,5 +9,5 @@ def common_elements(set_1, set_2):
     for i in set_1:
         for j in set_2:
             if i == j:
-                array += i
+                array.append(i)
     return array
