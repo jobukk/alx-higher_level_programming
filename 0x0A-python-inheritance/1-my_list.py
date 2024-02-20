e6bb4cdda9eba7147ce98acb91ba4d0fe6b7522f
JobKoech610 <jobkoech610@gmail.com>
2024-02-20 10:42:51 +0300

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,8 +10,17 @@ class MyList(list):
 
     def print_sorted(self):
         """Method that sort a list"""
-
-        sorted_list = sorted(list(self))
+        filtered_list = [x for x in self if x is not None]
+        sorted_list = sorted(list(filtered_list))
         print(sorted_list)
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)        
 
         
