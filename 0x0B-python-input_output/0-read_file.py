e515dff28e88a0261eac7dbfb9caa9c2eaba8bc6
JobKoech610 <jobkoech610@gmail.com>
2024-02-21 23:58:12 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Read file Modele"""
 
 
 def read_file(filename=""):
+    """Fucn to read file"""
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
