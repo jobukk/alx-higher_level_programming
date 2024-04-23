560d9b9e37bb7d16caf10091f2c1dbda3d8b0f56
JobKoech610 <jobkoech610@gmail.com>
2024-04-23 12:19:12 +0300

    fact

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 let value = Number(args[0]);
 
 function fact(value) {
-    if (value === 0 || value === 1) {
+    if (value === 0 || value === 1 || (isNaN(n))) {
         return 1;
     } else {
         return value * fact(value - 1);
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
+//  returns the addition of 2 integers.
 
 
-function add(a,b){
-    return(a+b)
-
-}
+exports.add = function (a, b) {
+    return a + b;
+  };
